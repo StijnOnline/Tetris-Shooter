6ff33438a0a033877b22907fa0d2fec1bf931aa0
StijnOnline <stijnvandeijzen@gmail.com>
2019-09-03 21:46:28 +0200

    Created a working basis

---
+++ Tetris Shooter/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private Player player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+}
